0720a2b3ddff902ffa4078b1f949f4ae0942ae83
Levent Ali <levent@purebreeze.com>
2020-07-19 06:17:36 +0100

    tai11s

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./components/**/*.js', './pages/**/*.js'],
+  theme: {
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Poppins', 'sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
