b0cd0b06a5163c3479d3ed25581174230e9c43f1
Levent Ali <levent.ali@intercom.io>
2020-07-20 06:02:41 +0100

    Improve typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   purge: false,
-  theme: {},
+  theme: {
+    fontFamily: {
+      sans:
+        'Avenir, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      serif: 'Georgia, Cambria, "Times New Roman", Times, serif'
+    }
+  },
   variants: {},
   plugins: []
 };
